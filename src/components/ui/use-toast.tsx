756daade03431838aea14c226d7f56ff81592d2e
Collins Lekakeny <lekakenycollins8@gmail.com>
2025-07-09 19:01:21 +0300

    refactor: update ToasterToast type definition with new props from sonner package

--- src/components/ui/use-toast.tsx
+++ src/components/ui/use-toast.tsx
@@ -2,18 +2,17 @@
 
 import * as React from "react";
 
-import type {
-  Toaster
-} from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
+import type { ToasterProps } from "sonner";
 
 const TOAST_LIMIT = 5;
 const TOAST_REMOVE_DELAY = 1000000;
 
-type ToasterToast = Toaster & {
+type ToasterToast = ToasterProps & {
   id: string;
-  title?: React.ReactNode;
-  description?: React.ReactNode;
-  action?: ToastActionElement;
+  action?: React.ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
 };
 
 const actionTypes = {
