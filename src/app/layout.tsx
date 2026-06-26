dc4452d16584490259415b4c213be86783f1b15b
Collins Lekakeny <lekakenycollins8@gmail.com>
2026-06-26 23:41:26 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ import Footer from "@/components/layout/Footer";
 // Import AdminLayoutWrapper
 import AdminLayoutWrapper from "@/components/admin/AdminLayoutWrapper";
 
-// Import all SEO components
+// Import SEO components
 import OrganizationSchema from "@/components/seo/OrganizationSchema";
 
 const geistSans = Geist({
